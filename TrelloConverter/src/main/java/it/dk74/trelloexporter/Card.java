f2d9aa9cfe9ac0a03822a2a446ff07432d82264c
darknight74 <darknight74@gmail.com>
2015-07-22 17:10:01 +0100

    First version of Card deserializer

--- TrelloConverter/src/main/java/it/dk74/trelloexporter/Card.java
+++ TrelloConverter/src/main/java/it/dk74/trelloexporter/Card.java
@@ -4,26 +4,59 @@ import java.util.Date;
 
 public class Card {
 
-	private String mTitle;
-	private Date mdueDate;
-	private String mDescription;
+	private String mTitle = "";
+	private String mdueDate = "";
+	private String mDescription = "";
 		
 	public String getTitle() {
 		return mTitle;
 	}
-	public void setTitle(String mTitle) {
-		this.mTitle = mTitle;
+	
+	public void setTitle(String title) {
+		if (title != null) {
+			this.mTitle = title;
+		}
 	}
-	public Date getDueDate() {
+	
+	public String getDueDate() {
 		return mdueDate;
 	}
-	public void setDueDate(Date mdueDate) {
-		this.mdueDate = mdueDate;
+	
+	public void setDueDate(String dueDate) {
+		if(dueDate != null) {
+			this.mdueDate = dueDate;
+		}
 	}
+	
 	public String getDescription() {
 		return mDescription;
 	}
-	public void setDescription(String mDescription) {
-		this.mDescription = mDescription;
+	
+	public void setDescription(String description) {
+		if (description != null) {
+			this.mDescription = description;
+		}
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		boolean result = false;
+		if (obj instanceof Card) {
+			Card test = (Card) obj;
+			result = (mTitle.equals(test.getTitle()) &&
+					mDescription.equals(test.getDescription()) &&
+						mdueDate.equals(test.getDueDate()));
+		}
+		return result;
+		
+	}
+	
+	@Override
+	public String toString() {
+		return "{title: \"" + mTitle + "\"; " +  
+				"description: \"" + mDescription + "\"; " + 
+				"dueDate: \"" + mdueDate + "\"}";
+	}
+	
+	
 }
