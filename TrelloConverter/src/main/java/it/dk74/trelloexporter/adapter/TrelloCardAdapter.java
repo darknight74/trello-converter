a1f616d1ea092c7c7c3948fc207b2f33f6988dcf
darknight74 <darknight74@gmail.com>
2015-07-22 20:36:55 +0100

    Moved to specific package

---
+++ TrelloConverter/src/main/java/it/dk74/trelloexporter/adapter/TrelloCardAdapter.java
@@ -0,0 +1,61 @@
+package it.dk74.trelloexporter.adapter;
+
+import it.dk74.trelloexporter.Card;
+
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonToken;
+import com.google.gson.stream.JsonWriter;
+
+public class TrelloCardAdapter extends TypeAdapter<Card> {
+	private static Logger LOG = LogManager.getLogger(TrelloCardAdapter.class);
+
+	@Override
+	public void write(JsonWriter out, Card value) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+	@Override
+	public Card read(JsonReader in) throws IOException {
+		final String logHead = "read - ";
+		LOG.info(logHead + "start method");
+		Card out = new Card();
+		in.beginObject();
+		while (in.hasNext()) {
+			String key = in.nextName();
+			String value;
+			if (key.equals("desc")) {
+				value = in.nextString();
+				LOG.debug(logHead + "<desc> key found; value [" + value + "]");
+				out.setDescription(value);
+			} else if (key.equals("name")) {
+				value = in.nextString();
+				LOG.debug(logHead + "<name> key found; value [" + value + "]");
+				out.setTitle(value);
+			} else if(key.equals("due") && in.peek() != JsonToken.NULL) {
+				value = in.nextString();
+				LOG.debug(logHead + "<due> key found; value [" + value + "]");
+				out.setDueDate(value);
+			} else if (key.equals("idList")) {
+				value = in.nextString();
+				LOG.debug(logHead + "<idList> key found; value [" + value + "]");
+				out.setIdList(value);
+			} else {
+				in.skipValue();
+			}
+				
+		}
+		in.endObject();
+		LOG.info(logHead + "end method");
+		return out;
+	}
+
+}
